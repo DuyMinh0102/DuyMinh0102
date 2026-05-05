79bd5cc66a84f5fd1c9e7817648fdaf4e9fedb6c
minhh <minhnguyenduy1234@gmail.com>
2026-05-05 07:12:29 +0700

    updates

---
+++ Competitive_Programming/Train/DP_CF/C_GeorgeAndJob/main.cpp
@@ -0,0 +1,36 @@
+// Source: https://codeforces.com/contest/467/problem/C
+// Status:
+/*
+Note: 
+
+Solution:
+
+Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
+Date: 04/05/2026 (DD/MM/YYYY).
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Debug
+#define dbg(testingLine, debugDescription, testedVariable) cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' ' << testedVariable << '\n'; 
+
+// Config
+#define pb push_back
+#define ll long long
+const int N = 5005;
+
+// Variables
+int n, m, k;
+ll a[N];
+ll dp;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    
+}
