5943de07f6814d4419581fb5765bedd3a9fa87b9
Duy Minh <minhnguyenduy1234@gmail.com>
2026-05-09 11:34:37 +0700

    Updates: 11:34 May 9 2026

---
+++ Competitive_Programming/Train/DP_CF/B_ColorTheFence/main.cpp
@@ -0,0 +1,69 @@
+// Source: https://codeforces.com/contest/349/problem/B
+// Status: Pending
+/*
+Note: 
+
+Solution:
+
+Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
+Date: 07/05/2026 (DD/MM/YYYY).
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Debug
+#define dbg(testingLine, debugDescription, testedVariable) cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' ' << testedVariable << '\n';
+
+// Config
+#define pb push_back
+#define ll long long
+
+// Variables
+int v, a[10];
+
+struct cmp{
+    bool operator()(pair<int, int> a, pair<int, int> b){
+        if (a.first != b.first) return a.first > b.first;
+
+        return a.second < b.second;
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> v;
+    bool paintable = false;
+    priority_queue<pair<int, int>, vector<pair<int,int>>, cmp> pq;
+
+    for (int i = 1; i <= 9; ++i){
+        cin >> a[i];
+        if (a[i] <= v){
+            paintable = true;
+            pq.push(make_pair(a[i],i));
+        }
+    }
+
+    if(!paintable){
+        cout << -1;
+        return 0;
+    }
+
+    int u = pq.top().second, p = pq.top().first;
+    int amt = v / p;    
+    string res(amt, (char)(u + '0'));   
+    v %= p;
+
+    for (int i = 0; i < amt; ++i){
+        for (int d = 9; d > u; --d){
+            if (v + p >= a[d]){
+                res[i] = (char)(d + '0');
+                v -= (a[d] - p);
+                break;
+            }
+        }
+    }
+
+    cout << res;
+}
