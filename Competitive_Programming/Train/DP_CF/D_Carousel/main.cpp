0510b49e72c79368841aea0f7eabcb178c16b4a5
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-05-09 18:28:41 +0700

    Updates_18:28_May 09 2026

---
+++ Competitive_Programming/Train/DP_CF/D_Carousel/main.cpp
@@ -0,0 +1,26 @@
+// Source: https://codeforces.com/contest/1328/problem/D
+// Status: Pending
+/*
+Note:  
+
+Solution:
+
+Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
+Date: 09/05/2026 (DD/MM/YYYY).
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Debug
+#define dbg(testingLine, debugDescription, testedVariable) cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' ' << testedVariable << '\n';
+
+// Config
+#define pb push_back
+#define ll long long
+
+// Variables
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
