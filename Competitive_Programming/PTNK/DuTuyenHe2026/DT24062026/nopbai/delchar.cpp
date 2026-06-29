34f9f061f2ce79b7816be333d71d6a047d590f6d
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-06-29 07:24:46 +0700

    June 29th 2026 - 07:24: Pushing unpushed works

--- Competitive_Programming/PTNK/DuTuyenHe2026/DT24062026/nopbai/delchar.cpp
+++ Competitive_Programming/PTNK/DuTuyenHe2026/DT24062026/nopbai/delchar.cpp
@@ -13,7 +13,6 @@ using namespace std;
 // Variables
 string s, t;
 bool possible = false;
-bool appeared[26];
 
 void solve() {
   cin >> s >> t;
@@ -21,41 +20,29 @@ void solve() {
   if (s[0] != t[0])
     return;
 
-  for (char c : t) {
-    appeared[c - 'a'] = true;
-  }
+  int id = 0;
+  for (char c : s) {
+    if (c == t[id]) {
+      ++id;
+      continue;
+    }
 
-  int lst_even = 0;
-  vector<char> v;
-  for (int i = 0; i < s.size(); ++i) {
-    ++lst_even;
-    if (i & 1)
-      lst_even = 1;
-    v.pb(s[i]);
+    if (possible)
+      continue;
 
-    if (!appeared[s[i] - 'a']) {
-      while (lst_even > 0 && !v.empty()) {
-        --lst_even;
-        v.pop_back();
-      }
+    if (id % 2 == 1) {
+      possible = true;
+    } else {
+      possible = true;
+      if (c != t[id])
+        --id;
     }
   }
 
-  string res = "";
-  for (char c : v)
-    res += c;
-
-  int id = 0;
-  string ans = "";
-  for (int i = 0; i < t.size(); ++i) {
-    while (res[id] != t[i])
-      ++id;
-    if (t[i] == res[id])
-      ans += res[id];
-  }
-
-  if (ans == t)
-    possible = true;
+  if (id == t.size())
+    cout << "YES";
+  else
+    cout << "NO";
 }
 
 int main() {
@@ -66,9 +53,4 @@ int main() {
   freopen("delchar.out", "w", stdout);
 
   solve();
-
-  if (possible)
-    cout << "YES";
-  else
-    cout << "NO";
 }
