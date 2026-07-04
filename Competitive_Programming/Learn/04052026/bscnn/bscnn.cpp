bc97829340d47599244fdf936381abbcd3163089
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-07-04 15:36:56 +0700

    update

--- Competitive_Programming/Learn/04052026/bscnn/bscnn.cpp
+++ Competitive_Programming/Learn/04052026/bscnn/bscnn.cpp
@@ -1,18 +1,20 @@
-// Source: 
-// Status:
+// Source:
+// Status: Unsolved
 /*
-Note: 
+Note:
 
 Solution:
 
 Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
-Date: (DD/MM/YYYY).
+Date: 11/05/2026 (DD/MM/YYYY).
 */
 #include <bits/stdc++.h>
 using namespace std;
 
 // Debug
-#define dbg(testingLine, debugDescription, testedVariable) cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' ' << testedVariable << '\n'; 
+#define dbg(testingLine, debugDescription, testedVariable)                     \
+  cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' '   \
+       << testedVariable << '\n';
 
 // Config
 #define pb push_back
@@ -22,9 +24,8 @@ const int N = 105;
 // Variables
 int n;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 }
--- Competitive_Programming/Learn/04052026/seq/seq.cpp
+++ Competitive_Programming/Learn/04052026/seq/seq.cpp
@@ -1,3 +1,13 @@
+// Source: No source
+// Status: Unsolved
+/*
+Note:
+
+Solution:
+
+Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
+Date: 22/05/2024 (DD/MM/YY)
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -5,42 +15,48 @@ const int N = 16500;
 int t, n;
 ll a[N], pref[N], res;
 
-ll recur(int l, int r){
-    if (l >= r) return 0;
-    ll cur_sum = pref[r] - pref[l - 1];
-    if (cur_sum == 0) return r - l;
-    if (cur_sum & 1) return 0;
+ll recur(int l, int r) {
+  if (l >= r)
+    return 0;
+  ll cur_sum = pref[r] - pref[l - 1];
+  if (cur_sum == 0)
+    return r - l;
+  if (cur_sum & 1)
+    return 0;
 
-    ll target = pref[l - 1] + (cur_sum / 2);
+  ll target = pref[l - 1] + (cur_sum / 2);
 
-    int it = lower_bound(pref + l, pref + r, target) - pref;
+  int it = lower_bound(pref + l, pref + r, target) - pref;
 
-    if (pref[it] != target) return 0;
+  if (pref[it] != target)
+    return 0;
 
-    return 1 + max(recur(l, it), recur(it + 1, r));
+  return 1 + max(recur(l, it), recur(it + 1, r));
 }
 
-void solve(){
-    res = 0;
-    cin >> n;
+void solve() {
+  res = 0;
+  cin >> n;
 
-    for (int i = 1; i <= n; ++i){
-        cin >> a[i];
-        pref[i] = pref[i - 1] + a[i];
-    }
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+    pref[i] = pref[i - 1] + a[i];
+  }
 
-    cout << recur(1, n);
+  cout << recur(1, n);
 }
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    freopen("seq.inp", "r", stdin);
-    freopen("seq.out", "w", stdout);
+  freopen("seq.inp", "r", stdin);
+  freopen("seq.out", "w", stdout);
 
-    cin >> t;
-    while(t--){
-        solve();
-        cout << '\n';
-    }
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
 }
