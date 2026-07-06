0d9eb9b8ed827a7cce69853cb8fa90df95a34270
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-07-06 09:43:04 +0700

    Solved problems plane and bitonic, source may be updated later

---
+++ Competitive_Programming/PTNK/DuTuyenHe2026/DT06072026/bitonic/bitonic.cpp
@@ -0,0 +1,53 @@
+// Source:
+// Status:
+/*
+Note:
+
+Solution:
+
+Author: Nguyen Duy Minh - studying at High School for the Gifted, VNU - HCM.
+Date: (DD/MM/YYYY).
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Debug
+#define dbg(testingLine, debugDescription, testedVariable)                     \
+  cerr << "Debug on line " << testingLine << ': ' << debugDescription << ' '   \
+       << testedVariable << '\n';
+
+// Config
+#define pb push_back
+#define ll long long
+const int N = 300005;
+
+// Variables
+int n, a[N];
+vector<ll> L(N, 1), R(N, 1);
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  freopen("bitonic.inp", "r", stdin);
+  freopen("bitonic.out", "w", stdout);
+
+  cin >> n;
+
+  for (int i = 1; i <= n; ++i)
+    cin >> a[i];
+
+  for (int i = 2; i <= n; ++i)
+    if (a[i] > a[i - 1])
+      L[i] = L[i - 1] + 1;
+
+  for (int i = n - 1; i > 0; --i)
+    if (a[i] > a[i + 1])
+      R[i] = R[i + 1] + 1;
+
+  ll total = 0;
+  for (int i = 1; i <= n; ++i)
+    total += L[i] * R[i];
+
+  cout << total;
+}
